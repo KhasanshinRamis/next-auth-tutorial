855c8634385e984a4e0bfd1c058cb2cd439812e8
Ramis <71335046+KhasanshinRamis@users.noreply.github.com>
2024-03-16 15:10:26 +0300

    fix style

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -7,7 +7,7 @@ interface ProtectedLayoutProps {
 
 export default async function ProtectedLayout({ children }: ProtectedLayoutProps) {
 	return (
-		<div className='w-full flex flex-col my-6 gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+		<div className='w-full flex flex-col py-6 gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
 			<Navbar />
 			{children}
 		</div>
